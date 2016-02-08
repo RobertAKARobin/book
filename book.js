c8772a54e984dadf2881bf5d679789eecbfa6e64
RobertAKARobin <robertgfthomas@gmail.com>
2016-02-07 20:12:51 -0500

    Interpolate blanks

--- book.js
+++ book.js
@@ -4,6 +4,10 @@ var fs = require("fs");
 var entities = new (require('html-entities').AllHtmlEntities)();
 
 var h = require("./public/helpers.js");
+var rx = {
+  code: /<#((.|[\n\r])*?)#>/g,
+  blank: /<\?((.|[\n\r])*?)\?>/g
+}
 
 app.use("/public", express.static(__dirname + "/public"));
 
@@ -13,8 +17,10 @@ app.get("/", function(req, res){
 
 app.get("/index.html", function(req, res){
   fs.readFile("./public/index.html", "utf8", function(err, html){
-    html = html.replace(/<#(([^#]|[\n\r])*)#>/g, function(match, p1){
+    html = html.replace(rx.code, function(match, p1){
       return entities.encode(p1);
+    }).replace(rx.blank, function(match, p1){
+      return "<span class='blank'>" + entities.encode(p1) + "</span>";
     });
     res.send(html);
   });
