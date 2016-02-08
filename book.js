08d9c852710f24fd5f610613ff1139045e575818
RobertAKARobin <robertgfthomas@gmail.com>
2016-02-07 19:37:20 -0500

    Different interpolation

--- book.js
+++ book.js
@@ -13,8 +13,8 @@ app.get("/", function(req, res){
 
 app.get("/index.html", function(req, res){
   fs.readFile("./public/index.html", "utf8", function(err, html){
-    html = html.replace(/(###)((.|[\n\r])*)(###)/gm, function(match, p1, p2){
-      return entities.encode(p2);
+    html = html.replace(/<#(([^#]|[\n\r])*)#>/g, function(match, p1){
+      return entities.encode(p1);
     });
     res.send(html);
   });
