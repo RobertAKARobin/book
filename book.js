432fd8dbf07ac90ebc004818f4080bcebefa2215
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-01 14:19:24 -0400

    Extracted replacer

--- book.js
+++ book.js
@@ -3,35 +3,10 @@
 var express = require("express");
 var app = express();
 var fs = require("fs");
-var entities = new (require('html-entities').AllHtmlEntities)();
 var h = require("helpers-js");
 
+var rx = require("./components/replacer");
 
-var rx = (function(){
-  var replacers = {};
-  var patterns = [
-    ["code", "#", "", ""],
-    ["blank", "?", '<span class="blank">', '</span>'],
-    ["mark", "*", "<mark>", "</mark>"]
-  ]
-  patterns.forEach(function(pattern){
-    var mark = pattern[1];
-    var regex = new RegExp("<\\" + mark + "((.|[\\n\\r])+?)\\" + mark + ">", "mg");
-    replacers[pattern[0]] = function(string){
-      return string.replace(regex, function(match, innerGroup){
-        return pattern[2] + entities.encode(innerGroup) + pattern[3];
-      });
-    }
-  });
-  replacers.replaceAll = function(string){
-    var type;
-    for(type in replacers){
-      if(type !== "replaceAll") string = replacers[type](string);
-    }
-    return string;
-  }
-  return replacers;
-}());
 fs.readFile("./node_modules/helpers-js/helpers.js", "utf8", function(a, data){
   fs.writeFile("./public/helpers.js", data);
 });
