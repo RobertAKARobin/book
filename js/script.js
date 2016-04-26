c6ddd7051056fa4bbd1b89cbfc6e886904745b35
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-26 10:59:35 -0400

    WIP: Replaces comments with code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Hello")
+"use strict";
+
+window.onload = function(){
+  
+  var codeBlocks  = document.querySelectorAll("[data-code]");
+  h.for_each(codeBlocks, function(codeBlock){
+    var childNodes = codeBlock.cloneNode(true).childNodes;
+    codeBlock.innerHTML = "";
+    h.for_each(childNodes, function(node){
+      var el = {};
+      if(node.nodeType === 8){
+        el = document.createElement("CODE");
+        el.textContent = node.textContent;
+      }else{
+        el = node.cloneNode(true);
+      }
+      codeBlock.appendChild(el);
+    });
+  });
+  
+}
