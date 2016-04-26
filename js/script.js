b83ae8eed33d8cecdc7f28ef6e8a7d376e9fe561
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-26 11:38:18 -0400

    Finished: Can add blanks and marks

--- js/script.js
+++ js/script.js
@@ -2,15 +2,25 @@
 
 window.onload = function(){
   
-  var codeBlocks  = document.querySelectorAll("[data-code]");
-  h.for_each(codeBlocks, function(codeBlock){
+  h.for_each(document.querySelectorAll("[data-code]"), function(codeBlock){
     var childNodes = codeBlock.cloneNode(true).childNodes;
     codeBlock.innerHTML = "";
     h.for_each(childNodes, function(node){
       var el = {};
       if(node.nodeType === 8){
-        el = document.createElement("CODE");
-        el.textContent = node.textContent;
+        switch(node.textContent.substring(0,1)){
+          case "_":
+            el = document.createElement("B");
+            el.textContent = node.textContent.substring(1);
+            break;
+          case "*":
+            el = document.createElement("MARK");
+            el.textContent = node.textContent.substring(1);
+            break;
+          default:
+            el = document.createElement("CODE");
+            el.textContent = node.textContent;
+        }
       }else{
         el = node.cloneNode(true);
       }
