aa3d944513f090408547d410e6538fcb0bb86d26
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-03 10:23:37 -0400

    Trying more modular replacer

--- js/script.js
+++ js/script.js
@@ -1,81 +1,114 @@
 "use strict";
 
-function Replacer(options){
-  var instance = this;
-  instance.matchers = Replacer.entityMatchers.slice();
-  if(options.block) h.for_each(options.block, function(tag, delimeter){
-    instance.matchers.push([
-      RegExp("(?:^|\\n|\\r)" + delimeter + " *(.*?)" + "(?:\\n|\\r|$)", "g"),
-      function(outer, inner){
-        return("\n\r<" + tag + ">" + inner + "</" + tag + ">\n\r");
-      }
-    ]);
-  });
-  if(options.inline) h.for_each(options.inline, function(tag, delimeter){
-    instance.matchers.push([
-      RegExp(delimeter + "(.*?)" + delimeter, "g"),
-      function(outer, inner){
-        return("<" + tag + ">" + inner + "</" + tag + ">");
-      }
-    ]);
-  });
+function Replacer(){}
+Replacer.tag = function(outer, inner, tag){
+  var tag = this;
+  return("<" + tag + ">" + inner + "</" + tag + ">");
 }
-Replacer.entityMatchers = (function(){
-  var entities = {
-    "<": "&lt;",
-    ">": "&gt;"
-  }
-  return h.collect(entities, function(entity, character){
-    return [RegExp(character, "g"), entity]
-  });
-})();
-Replacer.prototype.replace = function(container){
+Replacer.prototype.replaceElement  = function(element){
+  var instance  = this;
+  var output    = h.collect(
+    element.childNodes,
+    instance.replaceNode.bind(instance)
+  );
+  return output.join("");
+}
+Replacer.prototype.replaceNode = function(node){
   var instance = this;
-  var childNodes = container.cloneNode(true).childNodes;
-  container.innerHTML = "";
-  h.for_each(childNodes, function(node){
-    var html = node.textContent.replace(/(^[\n\r])|(\s*$)/g, "");
-    if(node.nodeType != 8){
-      html = node.outerHTML;
-    }else{
-      h.for_each(instance.matchers, function(pair){
-        html = html.replace(pair[0], pair[1]);
-      });
-    }
-    container.innerHTML += html;
+  var input;
+  switch(node.nodeType){
+    default:  // element
+      input = node.outerHTML;
+      break;
+    case 3:   // text
+      input = node.textContent;
+      break;
+    case 8:   // comment
+      input = node.textContent;
+      input = instance.swapEntities(input);
+      input = instance.swapMultilines(input);
+      input = instance.swapSingleLines(input);
+      input = instance.swapInlines(input);
+      break;
+  }
+  return input;
+}
+Replacer.prototype.swapEntities = function(input){
+  var instance  = this;
+  h.for_each(instance.matchers.entities, function(entity, character){
+    var regex = RegExp(character, "gm");
+    input = input.replace(regex, entity);
   });
+  return input;
+}
+Replacer.prototype.swapMultilines = function(input){
+  var instance  = this;
+  h.for_each(instance.matchers.multiline, function(tag, regex){
+    var regex = RegExp(regex, "gm");
+    input = input.replace(regex, Replacer.tag.bind(tag));
+  });
+  return input;
+}
+Replacer.prototype.swapSingleLines = function(input){
+  var instance  = this;
+  var matchers  = [];
+  var output    = "";
+  h.for_each(instance.matchers.singleline, function(tag, delimeter){
+    matchers.push([
+      RegExp("^ *" + delimeter + " *(.*?)$", "g"),
+      Replacer.tag.bind(tag)
+    ]);
+  });
+  h.for_each(input.split(/[\n\r]/g), function(line){
+    h.for_each(matchers, function(pair){
+      line = line.replace(pair[0], pair[1]);
+    });
+    if(line.substring(0,1) != "<"){
+      line = Replacer.tag.call("p", "", line);
+    }
+    output += line;
+  });
+  return output;
+}
+Replacer.prototype.swapInlines = function(input){
+  var instance  = this;
+  h.for_each(instance.matchers.inline, function(tag, delimeter){
+    var regex = RegExp(delimeter + "(.*?)" + delimeter, "g");
+    input = input.replace(regex, Replacer.tag.bind(tag));
+  });
+  return input;
+}
+Replacer.prototype.matchers = {
+  entities: {
+    "\\&":        "&amp;",
+    "\\<":        "&lt;",
+    "\\>":        "&gt;"
+  },
+  multiline: {
+    "```([\\s\\S]*)```":  "pre" // \s\S matches across lines
+  },
+  singleline: {
+    "#{4}":     "h4",
+    "#{3}":     "h3",
+    "#{2}":     "h2",
+    "#{1}":     "h1",
+    "''''":     "blockquote"
+  },
+  inline: {
+    "___":      "i",
+    "__":       "b",
+    "`":        "code",
+    "\\*\\*":   "strong",
+    "\\*":      "em",
+    "''":       "q",
+    "~~":       "mark"
+  }
 }
 
 window.onload = function(){
-  
-  var codeReplacer = new Replacer({
-    inline: {
-      "___":      "i",
-      "__":       "b",
-      "\\*\\*":   "mark"
-    }
-  });
-  var mdReplacer = new Replacer({
-    inline: {
-      "`":        "code",
-      "\\*\\*":   "strong",
-      "\\*":      "em",
-      "\\\\\\\\": "q"
-    },
-    block: {
-      "#{4}":     "h4",
-      "#{3}":     "h3",
-      "#{2}":     "h2",
-      "#{1}":     "h1",
-      "=":        "blockquote",
-      " +": "p"
-    }
-  });
-
-  h.for_each(document.querySelectorAll("[data-code]"), function(node){
-    codeReplacer.replace(node);
-  });
-  h.for_each(document.querySelectorAll("[data-md]"), function(node){
-    mdReplacer.replace(node);
+  var MD = new Replacer();
+  h.for_each(document.querySelectorAll("[data-md]"), function(el){
+    window.test = (MD.replaceElement(el));
+    console.log(window.test)
   });
 }
