347d0b91524e00823dbfc98a3f02cb8598905bc8
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-03 20:35:48 -0400

    Replacer goes line-by-line

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ Replacer.tag = function(outer, inner, tag){
   var tag = this;
   return("<" + tag + ">" + inner + "</" + tag + ">");
 }
-Replacer.prototype.replaceElement  = function(element){
+Replacer.prototype.replaceElement = function(element){
   var instance  = this;
   var output    = h.collect(
     element.childNodes,
@@ -14,101 +14,81 @@ Replacer.prototype.replaceElement  = function(element){
   return output.join("");
 }
 Replacer.prototype.replaceNode = function(node){
-  var instance = this;
-  var input;
+  var instance  = this;
+  var output;
   switch(node.nodeType){
     default:  // element
-      input = node.outerHTML;
+      output    = node.outerHTML;
       break;
     case 3:   // text
-      input = node.textContent;
+      output    = node.textContent;
       break;
     case 8:   // comment
-      input = node.textContent;
-      input = instance.swapEntities(input);
-      input = instance.swapMultilines(input);
-      input = instance.swapSingleLines(input);
-      input = instance.swapInlines(input);
+      output    = node.textContent;
+      output    = instance.replaceText(output);
       break;
   }
-  return input;
+  return output;
 }
-Replacer.prototype.swapEntities = function(input){
+Replacer.prototype.replaceText = function(text){
   var instance  = this;
-  h.for_each(instance.matchers.entities, function(entity, character){
-    var regex = RegExp(character, "gm");
-    input = input.replace(regex, entity);
-  });
-  return input;
+  var output    = h.collect(
+    text.split(/[\n\r]/g),
+    instance.replaceLine.bind(instance)
+  );
+  return output.join("\n\r");
 }
-Replacer.prototype.swapMultilines = function(input){
+Replacer.prototype.replaceLine = function(line){
   var instance  = this;
-  h.for_each(instance.matchers.multiline, function(tag, regex){
-    var regex = RegExp(regex, "gm");
-    input = input.replace(regex, Replacer.tag.bind(tag));
+  var output    = line;
+  h.for_each(instance.matchers.entities, function(matcher){
+    matcher[2]  = (matcher[2] || RegExp(matcher[0], "g"));
+    output      = output.replace(matcher[2], matcher[1]);
   });
-  return input;
-}
-Replacer.prototype.swapSingleLines = function(input){
-  var instance  = this;
-  var matchers  = [];
-  var output    = "";
-  h.for_each(instance.matchers.singleline, function(tag, delimeter){
-    matchers.push([
-      RegExp("^ *" + delimeter + " *(.*?)$", "g"),
-      Replacer.tag.bind(tag)
-    ]);
+  h.for_each(instance.matchers.singleline, function(matcher){
+    var original= output;
+    matcher[2]  = (matcher[2] || RegExp("^" + matcher[0] + "\\s*(.*)", "g"));
+    matcher[3]  = (matcher[3] || Replacer.tag.bind(matcher[1]));
+    output      = output.replace(matcher[2], matcher[3]);
+    if(original !== output) return "break";
   });
-  h.for_each(input.split(/[\n\r]/g), function(line){
-    h.for_each(matchers, function(pair){
-      line = line.replace(pair[0], pair[1]);
-    });
-    if(line.substring(0,1) != "<"){
-      line = Replacer.tag.call("p", "", line);
-    }
-    output += line;
+  h.for_each(instance.matchers.inline, function(matcher){
+    matcher[2]  = (matcher[2] || RegExp(matcher[0] + "(.*?)" + matcher[0], "g"));
+    matcher[3]  = (matcher[3] || Replacer.tag.bind(matcher[1]));
+    output      = output.replace(matcher[2], matcher[3]);
   });
   return output;
 }
-Replacer.prototype.swapInlines = function(input){
-  var instance  = this;
-  h.for_each(instance.matchers.inline, function(tag, delimeter){
-    var regex = RegExp(delimeter + "(.*?)" + delimeter, "g");
-    input = input.replace(regex, Replacer.tag.bind(tag));
-  });
-  return input;
-}
 Replacer.prototype.matchers = {
-  entities: {
-    "\\&":        "&amp;",
-    "\\<":        "&lt;",
-    "\\>":        "&gt;"
-  },
-  multiline: {
-    "```([\\s\\S]*)```":  "pre" // \s\S matches across lines
-  },
-  singleline: {
-    "#{4}":     "h4",
-    "#{3}":     "h3",
-    "#{2}":     "h2",
-    "#{1}":     "h1",
-    "''''":     "blockquote"
-  },
-  inline: {
-    "___":      "i",
-    "__":       "b",
-    "`":        "code",
-    "\\*\\*":   "strong",
-    "\\*":      "em",
-    "''":       "q",
-    "~~":       "mark"
-  }
+  entities: [
+    ["\\&",   "&amp;"],
+    ["\\<",   "&lt;"],
+    ["\\>",   "&gt;"]
+  ],
+  singleline: [
+    ["#{4}",  "h4"],
+    ["#{3}",  "h3"],
+    ["#{2}",  "h2"],
+    ["#{1}",  "h1"],
+    ["''''",  "blockquote"],
+    ["",      "p"]
+  ],
+  inline: [
+    ["_{3}",  "i"],
+    ["_{2}",  "b"],
+    ["`{1}",  "code"],
+    ["\\*{2}","strong"],
+    ["\\*{1}","em"],
+    ["'{2}",  "q"],
+    ["~{2}",  "mark"]
+  ]
 }
 
 window.onload = function(){
   var MD = new Replacer();
   h.for_each(document.querySelectorAll("[data-md]"), function(el){
     window.test = (MD.replaceElement(el));
+    el.innerHTML = window.test;
     console.log(window.test)
   });
 }
