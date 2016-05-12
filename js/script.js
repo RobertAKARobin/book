4d76d4eae4e4036176f988428dad9262b16ceff1
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-12 16:08:29 -0400

    Commented data-code back in

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-// "use strict";
-// 
-// window.onload = function(){
-//   h.for_each(document.querySelectorAll("section"), function(el){
-//     el.innerHTML = MD.replaceElement(el);
-//   });
-//   h.for_each(document.querySelectorAll("[data-code-cols]"), function(el){
-//     var indexes = el.getAttribute("data-code-cols").split(/ +/g);
-//     h.for_each(el.querySelectorAll("tr"), function(row){
-//       h.for_each(row.children, function(td, index){
-//         if(indexes.includes(index.toString())){
-//           td.setAttribute("data-code", true);
-//         }
-//       });
-//     });
-//   });
-// }
+"use strict";
+
+window.onload = function(){
+  h.for_each(document.querySelectorAll("[data-code-cols]"), function(el){
+    var indexes = el.getAttribute("data-code-cols").split(/ +/g);
+    h.for_each(el.querySelectorAll("tr"), function(row){
+      h.for_each(row.children, function(td, index){
+        if(indexes.includes(index.toString())){
+          td.setAttribute("data-code", true);
+        }
+      });
+    });
+  });
+}
