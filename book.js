59ba34e50879dbe5dfa9da5f72268f90ba603615
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-12 11:44:50 -0400

    WIP: Initialized Node

---
+++ book.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var fs    = require("fs");
+var h     = require("./js/helpers.js");
+
+(function loadPages(){
+  
+  var filenames = require("./pages/_index");
+  h.for_each(filenames, function(filename, i){
+    var file  = read("./pages/" + filename + ".md");
+    
+  });
+  
+})();
+
+(function insertPages(){
+  
+  var index     = read("index.html").split("{{{yield}}}");
+  
+})();
+
+function read(filename){
+  return fs.readFileSync(filename, "utf8");
+}
+
+function write(filename){
+  return fs.writeFileSync(filename, "utf8");
+}
