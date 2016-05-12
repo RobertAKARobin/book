ec7d5d5b353f6220cd884b57f3479f2d087bc3c7
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-12 18:50:56 -0400

    JS styles blanks inside tds

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 "use strict";
 
 window.onload = function(){
-  h.for_each(document.querySelectorAll("[data-code-cols]"), function(el){
+  h.for_each(h.el("[data-code-cols]"), function(el){
     var indexes = el.getAttribute("data-code-cols").split(/ +/g);
-    h.for_each(el.querySelectorAll("tr"), function(row){
+    h.for_each(h.el("tr", el), function(row){
       h.for_each(row.children, function(td, index){
         if(indexes.includes(index.toString())){
           td.setAttribute("data-code", true);
         }
       });
     });
+  });
+  
+  h.for_each(h.el("td b"), function(el){
+    if(!el.previousSibling && !el.nextSibling){
+      el.classList.add("line");
+    }
   });
 }
