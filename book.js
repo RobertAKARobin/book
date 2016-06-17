65eed2269d421887b8b216c475aa7856bdd84c8a
RobertAKARobin <robertgfthomas@gmail.com>
2016-06-17 16:29:14 -0400

    Added IDs and anchors

--- book.js
+++ book.js
@@ -4,10 +4,6 @@ var fs    = require("fs");
 var beaut = require("js-beautify");
 var h     = require("./js/helpers.js");
 var layout= read("layouts/main.html").split("{{{yield}}}");
-var div   = {
-  open: "<div class=\"section\">",
-  close: "</div>"
-}
 var pages = [];
 var matchers  = require("./js/markymarkdown.js");
 var filenames = [];
@@ -16,11 +12,11 @@ read("./pages/_index.csv").split(/[\n\r]/).forEach(function(line, i){
   var data;
   if(!line || line == "") return;
   data = line.split(/ *# */)
-  filenames.push({path: data[0], title: data[1] });
+  filenames.push({id: data[0], title: data[1] });
 });
 
 filenames.forEach(function(filename, i){
-  var file    = read("./pages/" + filename.path + ".html");
+  var file    = read("./pages/" + filename.id + ".html");
   var content = [];
   var contentString = "";
   file.split(/[\n\r]/g).forEach(function(line){
@@ -35,11 +31,14 @@ filenames.forEach(function(filename, i){
     content.push(line);
   });
   contentString = content.join("\n");
+  contentString = "<div id=\"" + filename.id + "\" class=\"section\">" + contentString + "</div>";
+  contentString = contentString.replace("{{title}}", function(){
+    return "<a href=\"#" + filename.id + "\">" + filename.title + "</a>";
+  });
   pages.push(contentString);
-  // writePage("page_" + filename + ".html", contentString);
 });
 
-writePage("index.html", pages.join(div.close + div.open));
+writePage("index.html", pages.join("\n"));
 
 function read(filename){
   return fs.readFileSync(filename, "utf8");
@@ -50,7 +49,7 @@ function write(filename, content){
 }
 
 function writePage(filename, content){
-  var output    = layout[0] + div.open + content + div.close + layout[1];
+  var output    = layout[0] + content + layout[1];
   write(filename, beaut.html(output, {
     indent_size: 2,
     end_with_newline: true
