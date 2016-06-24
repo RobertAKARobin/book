b205a5a0eee40fabc3ecc4d4f9ef3bbe298fc6b9
RobertAKARobin <robertgfthomas@gmail.com>
2016-06-24 17:24:09 -0400

    Organized by section with page numbers

--- book.js
+++ book.js
@@ -4,40 +4,30 @@ var fs        = require("fs");
 var beaut     = require("js-beautify");
 var matchers  = require("./js/markymarkdown.js");
 
-var layout    = read("layouts/main.html").split("{{{yield}}}");
-var filenames = [];
 var book      = "";
+var layout    = {
+  main: read("./layouts/main.html"),
+  page: read("./layouts/page.html")
+}
+var viewVars  = {
+  pageNum:      0,
+  sectionName:  null
+}
 
-read("./pages/_index.csv").split(/[\n\r]/).forEach(function(line, i){
-  var data;
-  if(!line || line == "") return;
-  data = line.split(/ *# */)
-  filenames.push({id: data[0], title: data[1] });
-});
-
-filenames.forEach(function(filename, i){
-  var file = read("./pages/" + filename.id + ".html");
-  var page = "";
-  file.split(/[\n\r]/g).forEach(function(line){
-    try{matchers.singleline.forEach(function(matcher){
-      var raw = line;
-      line    = matcher(line);
-      if(line !== raw) throw "This is a cheat to let me 'break' the loop.";
-    })}catch(e){};
-    matchers.inline.forEach(function(matcher){
-      line    = matcher(line);
+require("./sections/_index").forEach(function(section){
+  viewVars.sectionName = section;
+  read("./sections/" + section + ".html").split(/\s*={5}\s*/).forEach(function(page){
+    viewVars.pageNum += 1;
+    page = markyMark(page);
+    page = layout.page.replace("{{body}}", page);
+    page = page.replace(/\{\{(.*?)}}/g, function(nil, varName){
+      return viewVars[varName];
     });
-    page += line + "\n";
+    book += page;
   });
-  page = page.replace("{{title}}", function(){
-    return "<a href=\"#" + filename.id + "\">" + filename.title + "</a>";
-  });
-  book += "<div id=\"" + filename.id + "\" class=\"section\">";
-  book += page.split("=====").join("</div><div class=\"section\">");
-  book += "</div>";
 });
 
-writePage("index.html", book);
+writePage("index.html", layout.main.replace("{{body}}", book));
 
 function read(filename){
   return fs.readFileSync(filename, "utf8");
@@ -48,9 +38,26 @@ function write(filename, content){
 }
 
 function writePage(filename, content){
-  var output    = layout[0] + content + layout[1];
-  write(filename, beaut.html(output, {
+  write(filename, beaut.html(content, {
     indent_size: 2,
     end_with_newline: true
   }));
+}
+
+function markyMark(input){
+  var output = "";
+  input.split(/[\n\r]/g).forEach(function(line){
+    try{
+      matchers.singleline.forEach(function(matcher){
+        var raw = line;
+        line = matcher(line);
+        if(line !== raw) throw "break";
+      });
+    }catch(e){ /*This is a cheat to let me 'break' the loop.*/ }
+    matchers.inline.forEach(function(matcher){
+      line = matcher(line);
+    });
+    output += line + "\n";
+  });
+  return output;
 }
